c61735948dd4d0cabdc7ff8bee0915c2418c5682
DoctorWolfy121 <williamswhitehouse@gmail.com>
2019-07-04 15:12:46 +0100

    Created Scripts

---
+++ Scripts/GravityItem.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class GravityItem : MonoBehaviour
+{
+    public Vector3 Up = Vector3.up;
+    public int ActiveFieldCount;
+    public float CurrentDistance = Mathf.Infinity;
+    public GravitySource CurrentGravitySource;
+}
