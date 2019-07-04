efdd432c315e988d362122e920c5c5d8c1cb0213
DoctorWolfy121 <williamswhitehouse@gmail.com>
2019-07-04 15:55:05 +0100

    Created 2D versions of the Gravity Source and Item

---
+++ PlanetGravityExampleProject/Assets/Scripts/GravityItem2D.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class GravityItem2D : MonoBehaviour
+{
+    public Vector2 Up = Vector2.up;
+    public int ActiveFieldCount;
+    public float CurrentDistance = Mathf.Infinity;
+    public GravitySource2D CurrentGravitySource;
+}
