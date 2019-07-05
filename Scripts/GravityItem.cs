97ddbab778369fac4ee7e2f0054d34c509977f9a
DoctorWolfy121 <williamswhitehouse@gmail.com>
2019-07-05 11:58:43 +0100

    Removed unused scripts

--- Scripts/GravityItem.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-public class GravityItem : MonoBehaviour
-{
-    public Vector3 Up = Vector3.up;
-    public int ActiveFieldCount;
-    public float CurrentDistance = Mathf.Infinity;
-    public GravitySource CurrentGravitySource;
-}
--- Scripts/GravityItem2D.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-public class GravityItem2D : MonoBehaviour
-{
-    public Vector2 Up = Vector2.up;
-    public int ActiveFieldCount;
-    public float CurrentDistance = Mathf.Infinity;
-    public GravitySource2D CurrentGravitySource;
-}
